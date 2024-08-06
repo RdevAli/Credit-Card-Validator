19a0516e9a6f1df1b29893c843e860c722f9266f
Ali Rehman <not9trogen@gmail.com>
2024-08-06 23:03:20 +0500

    feat: basic UI added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
-import React, { useState } from "react"; 
-import validator from 'validator'
-  
-const App = () => { 
-  
-  const [errorMessage, setErrorMessage] = useState('') 
-    
-  const validateCreditCard = (value) => { 
-    
-    if (validator.isCreditCard(value)) { 
-      setErrorMessage('Valid CreditCard Number') 
-    } else { 
-      setErrorMessage('Enter valid CreditCard Number!') 
-    } 
-  } 
-  
-  return ( 
-    <div style={{ 
-      display:"flex", justifyContent:"center", alignItems:"center" 
-    }}> 
-      <pre> 
-        <h2>Validating CreditCard in ReactJS</h2> 
-        <span>Enter CreditCard: </span><input type="text" 
-        onChange={(e) => validateCreditCard(e.target.value)}></input> <br /> 
-        <span style={{ 
-          fontWeight: 'bold', 
-          color: 'red', 
-        }}>{errorMessage}</span> 
-      </pre> 
-    </div> 
-  ); 
-} 
-  
-export default App
+import React, { useState } from "react";
+import validator from "validator";
+import card from "./assets/cc.png";
+import "./credform.css";
+
+const PaymentDetails = () => {
+  const [creditCardNumber, setCreditCardNumber] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const validateCreditCard = (value) => {
+    setCreditCardNumber(value);
+
+    if (validator.isCreditCard(value)) {
+      setIsValid(true);
+      setErrorMessage(""); // Clear the error message if the card is valid
+    } else {
+      setIsValid(false);
+      setErrorMessage("Enter a valid credit card number!"); // Set an error message if invalid
+    }
+  };
+
+  return (
+    <div className="payment-container">
+      <div className="card">
+        <div className="card-details">
+          <div className="card-header">
+            <h3>Payment details</h3>
+          </div>
+          <div className="card-body">
+            <div className="credit-card-image">
+              <img src={card} alt="Credit Card" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="creditCardNumber">Card number</label>
+              <input
+                type="text"
+                id="creditCardNumber"
+                value={creditCardNumber}
+                onChange={(e) => validateCreditCard(e.target.value)}
+                placeholder="1234 5678 9012 3456"
+              />
+              {errorMessage && (
+                <span className="error-message">{errorMessage}</span>
+              )}
+            </div>
+          </div>
+          <div className="card-footer">
+            <button className="save-button" disabled={!isValid}>
+              Save Changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentDetails;
